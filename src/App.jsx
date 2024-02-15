65427395a20f557337e8c26cd48fd7941feab36f
George Ion <89georgeion@gmail.com>
2024-02-15 22:07:52 +0200

    more curatenie

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/profile').then((response) => {
+      const { data } = response;
+
+      setName(data.name);
+    });
+  }, []);
+
   return (
-    <>
-      <h1>Hello </h1>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      {name.trim().length <= 0 ? '...loading' : name}
+    </h1>
   );
 }
 
