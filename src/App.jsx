91b7fbf1ad8d64d6b38b6f10492d940d41641322
George Ion <89georgeion@gmail.com>
2024-02-20 21:30:47 +0200

    env examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+const baseUrl = import.meta.env.VITE_SERVER_URI;
+
 function App() {
   const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3000/profile').then((response) => {
-      const { data } = response;
+    axios
+      .get(`${baseUrl}/profile`)
+      .then((response) => {
+        const { data } = response;
 
-      setName(data.name);
-    });
+        setName(data.name);
+      })
+      .catch((response) => {
+        const { code } = response;
+
+        if (code === 'ERR_NETWORK') {
+          setMessage('Could not fetch data');
+        }
+      });
   }, []);
+
+  if (message.trim().length > 0) {
+    return <div>{message}</div>;
+  }
 
   return (
     <h1 className="text-3xl font-bold underline">
